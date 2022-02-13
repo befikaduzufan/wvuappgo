8ee6a7db889870585e1c8bf989bbfd34047c4353
Befikadu <bk0023@mix.wvu.edu>
2022-02-13 14:03:07 -0500

    top-nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CategoryCardComponent } from './categories/category.card.component';
 import { CategoryHeaderComponent } from './category.header.component';
+import { TopNavBarComponenet } from './navigation/top-nav-bar.componenet';
 
 import { HorizontalCardComponent } from './store/horizontal.card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TopNavBarComponenet,
     CategoryCardComponent,
     CategoryHeaderComponent,
     HorizontalCardComponent
