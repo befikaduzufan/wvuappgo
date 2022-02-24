522969873a9ff807983454f8e580e92697672583
Befikadu <bk0023@mix.wvu.edu>
2022-02-23 21:07:37 -0500

    newone

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import { CourseProgramComponenet } from './store/courseprogram.component';
 import { HomeLayoutComponenet } from './store/home-layout.component';
 import { AllClassificationComponent } from './store/allclassification.component';
 import { ClassificationLayout } from './store/classificationlayout.component';
+import { Adventer } from './store/alladventer.component';
 const routes:Routes =[
   {path: '',component: HomeLayoutComponenet},
   {path: 'course-program',component: CourseProgramComponenet},
   {path: 'home',component: HomeLayoutComponenet},
   {path: 'All Classifications',component: ClassificationLayout},
-  {path: 'Adventure WV-Guided Trips',component:a}
+  {path: 'Adventure WV-Guided Trips',component:Adventer}
   
 
   
